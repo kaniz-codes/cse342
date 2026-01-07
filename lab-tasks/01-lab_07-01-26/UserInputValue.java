b5c677e2dbbfd2bf74f55a35bb09a69048fd45cd
kaniz-codes <social.kaniz@gmail.com>
2026-01-08 01:38:20 +0600

    Take user input

---
+++ lab-tasks/01-lab_07-01-26/UserInputValue.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class UserInputValue {
+    public static void main(String args[])
+    {
+        Scanner SC = new Scanner (System.in);
+        int a = SC.nextInt();
+        System.out.println("Value of a is "+ a);
+    }
+}
