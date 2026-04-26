75b67785a4b0d7d7e374ed5eb6a415e83fb24ab5
kaniz-codes <social.kaniz@gmail.com>
2026-04-27 00:06:19 +0600

    User-defined Exception

--- topics/ExceptionHandling/bankSystem.java
+++ topics/ExceptionHandling/bankSystem.java
@@ -1,4 +1,5 @@
-/*In a banking system, users perform transactions such as withdrawals. Errors may occur if the account number does not exist or the withdrawal amount exceeds the available balance.
+/*In a banking system, users perform transactions such as withdrawals.
+Errors may occur if the account number does not exist or the withdrawal amount exceeds the available balance.
 Write a Java program that uses both built-in and user-defined exceptions to handle these cases.
 Use throw, throws, try, catch and show exception propagation by calling a method that throws an exception and handling it in the main method.
 Write proper messages for each exception and continue processing remaining users even after an error occurs.
